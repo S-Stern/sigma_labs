2c41087ab1975523072c622e0178c79a646fd794
S-Stern <stern.sl@protonmail.com>
2024-08-25 10:46:59 +0100

    added boredom to l1c4t1

--- pre-course/L1-Challenge-4-Tasks-1-2.py
+++ pre-course/L1-Challenge-4-Tasks-1-2.py
@@ -3,3 +3,17 @@ def outed(meet, boss):
     if happyness <= 5:
         return 'Get Out Now!'
     return 'Nice Work Champ!'
+
+
+def boredom(staff):
+    score_dic = {"accounts": 1, "finance": 2, "canteen": 10, "regulation": 3, "trading": 6, "change": 6, "IS": 8, "retail": 5, "cleaning": 4, "pissing about": 25}
+    score = 0
+    for department in staff.values():
+        score += score_dic[department]
+        
+    if score <= 80:
+        return 'kill me now'
+    elif score < 100:
+        return 'i can handle this'
+    else:
+        return 'party time!!'
