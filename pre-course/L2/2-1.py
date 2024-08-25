e27cf0effd9d14ec388a4b301a3336221b2db3f5
S-Stern <stern.sl@protonmail.com>
2024-08-25 13:08:25 +0100

    .

--- pre-course/L2/2-1.py
+++ pre-course/L2/2-1.py
@@ -4,23 +4,39 @@ from sys import exit
 
 def game() -> tuple:
     ans = randint
-    last_guess = -float("inf")
+    last_guess = float("inf")
     guesses = 0
     win = False
 
     for i in range(1000):
-        guess = input("Please enter your next guess here: ")
+        guess = get_guess()
+        if not type(guess) == int:
+            break
         if guess == ans:
             win = True
             break
         if guess != last_guess:
             last_guess = guess
             guesses += 1
-        if guess < ans:
-            print("Sorry, your guess was too low.")
-        print("Sorry, your guess was too high.")
 
-    return (win, guesses)
+    exit("Apologies; you exceeded the guess limit and the program terminated.")
+
+
+def get_guess() -> tuple:
+    guess = input("Please enter your next guess here: ")
+    try:
+        return int(guess)
+    except:
+        print("Please ensure your guess is an integer.")
+        return guess
+
+
+def bad_guess(ans: int, guess: str) -> None:
+    if guess < ans:
+        print("Sorry, your guess was too low.")
+    else:
+        print("Sorry, your guess was too high.")
+    return
 
 
 def start() -> None:
